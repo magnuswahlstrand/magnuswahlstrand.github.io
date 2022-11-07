cf1bfc9153cfaab0298ba6c1ab3d79d8d172c0dd
Magnus <magnus@magnuss-mbp.lan>
2022-11-07 12:05:11 +0100

    try removing custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://wahlstrand.dev/",
+  site: "https://magnuswahlstrand.github.io",
   vite: { optimizeDeps: { include: ["asciinema-player"] } },
   integrations: [
     tailwind({
